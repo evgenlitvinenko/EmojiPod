f5e10012cf2e5885d111c36519fd7be829e71551
elitvinenko <ilitvinenko90@gmail.com>
2017-09-28 10:04:24 +0300

    Start position for pod

---
+++ EmojiPod/Classes/testClass.h
@@ -0,0 +1,14 @@
+//
+//  testClass.h
+//  EmojiPod
+//
+//  Created by Developer on 28.09.17.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testClass : UIView
+
+- (void)testLog;
+
+@end
---
+++ EmojiPod/Classes/testClass.m
@@ -0,0 +1,24 @@
+//
+//  testClass.m
+//  EmojiPod
+//
+//  Created by Developer on 28.09.17.
+//
+
+#import "testClass.h"
+
+@implementation testClass
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+- (void)testLog{
+    NSLog(@"testLog");
+}
+
+@end
